6a5ffd16a1309ddd04c4f7e42ddb88ffcca5387b
Nerissa-Javier <103042694+Nerissa-Javier@users.noreply.github.com>
2022-04-05 20:41:32 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<style>
+ body{
+  background-image: url('bg.jpg'); background-repeat: repeat-x;
+}
+</style>
+    
+    
+    <h3> Nerissa Liban Javier</h3>
+<break>
+<h3>Welcome to my Page</h3>
+<h4>Please feel fee to check sample PHP programs</h4>
+<ul>
+    <li><a href="http://localhost/project/sum.php"> PHP program to print sum of digits </a>
+    <li><a href="http://localhost/project/prime.php"> PHP Program To Check Prime Number </a>
+    <li><a href="http://localhost/project/table.php"> PHP program to print table of a number </a>
+    <li><a href="http://localhost/project/factorial.php"> PHP Program To Print Factorial Of A Number</a>
+
+</ul>
+    <body>
+ 
+<?php
+
+?>
+ 
+</body>
+</html>
